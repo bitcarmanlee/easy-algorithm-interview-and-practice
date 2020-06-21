1d6441d4b062ff528ef3549642f681e62a4e1e04
wanglei <bit_carmanlee@sina.com>
2020-06-21 19:47:59 +0800

    add tree code

---
+++ src/main/java/edu/bit/carmanlee/datastruct/tree/TreeNode.java
@@ -0,0 +1,13 @@
+package edu.bit.carmanlee.datastruct.tree;
+
+public class TreeNode {
+
+    int data;
+    TreeNode left, right;
+
+    public TreeNode(int data) {
+        this.data = data;
+    }
+
+
+}
